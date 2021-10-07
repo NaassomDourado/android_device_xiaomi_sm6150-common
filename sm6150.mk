15c7f8bf3a72eeb0796e9d25660d2c1f67e29231
Pavel Dubrova <pashadubrova@gmail.com>
2021-10-07 10:39:12 +0300

    sm6150-common: Provide android.frameworks.sensorservice@1.0 on vendor
    Following the commit f19703e ("common-treble: Provide radio, keymaster
    and secure_element HAL on vendor"), starting with Android 12 most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        QCamera : <MCI><ERROR> mm_camera_load_shim_lib: 3306: dlopen failed
        with error dlopen failed: library
        "android.frameworks.sensorservice@1.0.so" not found: needed by
        /odm/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    android.frameworks.sensorservice@1.0 is used by at least mm-camera
    framework.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>
    Change-Id: I72d9bcda1fdfd48c5fb9cb21666edd995ae4d9af
    
    Update sm6150.mk

--- sm6150.mk
+++ sm6150.mk
@@ -94,6 +94,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor
 
